1d0874529cbae8bd35a34d8a730f27e9d8d85893
shuyshuys <ivlyns@protonmail.com>
2023-11-24 16:12:05 +0700

    finalize note editing process until success

--- lib/edit_page.dart
+++ lib/edit_page.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class EditPage extends StatefulWidget {
   final String id; // Add id parameter to the constructor
@@ -13,6 +11,7 @@ class EditPage extends StatefulWidget {
 }
 
 class _EditPageState extends State<EditPage> {
+  final TextEditingController _updateController = TextEditingController();
   final db = FirebaseFirestore.instance;
   String noteValue = '';
 
@@ -28,24 +27,26 @@ class _EditPageState extends State<EditPage> {
           .collection('noteapp')
           .doc(widget.id)
           .get(); // Use widget.id to access the id parameter
-      if (snapshot.exists) {
+      final data = snapshot.data() as Map<String, dynamic>;
+      final note = data['nama'];
+      if (note != null) {
         setState(() {
-          noteValue = (snapshot.data() as Map<String, dynamic>)['note'];
+          noteValue = note;
         });
       }
     } catch (e) {
-      print('Error fetching note value: $e');
+      // print('Error fetching note value: $e');
     }
   }
 
   Future<void> updateNoteValue(String newValue) async {
     try {
-      await db.collection('noteapp').doc(widget.id).update({'note': newValue});
+      await db.collection('noteapp').doc(widget.id).update({'nama': newValue});
       setState(() {
         noteValue = newValue;
       });
     } catch (e) {
-      print('Error updating note value: $e');
+      // print('Error updating note value: $e');
     }
   }
 
@@ -53,19 +54,35 @@ class _EditPageState extends State<EditPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Edit Page'),
+        title: const Text('Halaman Ubah Note'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Note Value: $noteValue'),
+            Text('Note berisi $noteValue'),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: 100), // Add padding on the right and left
+              child: TextField(
+                controller: _updateController,
+                decoration: const InputDecoration(
+                  hintText: 'Masukkan perubahan nama disini',
+                ),
+              ),
+            ),
             ElevatedButton(
               onPressed: () {
-                // Call the updateNoteValue method with the new value
-                updateNoteValue('New Note Value');
+                if (_updateController.text.isEmpty) return;
+
+                updateNoteValue(_updateController.text);
               },
-              child: Text('Update Note Value'),
+              // onPressed: () {
+              //   // Call the updateNoteValue method with the new value
+              //   updateNoteValue('New Note Value');
+              // },
+              child: const Text('Update isi Note'),
             ),
           ],
         ),
