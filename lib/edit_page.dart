647aeb7cb95c6873cad139978547e02a2f274e61
shuyshuys <ivlyns@protonmail.com>
2023-11-24 14:10:49 +0700

    edit still error, lil bit more

--- lib/edit_page.dart
+++ lib/edit_page.dart
@@ -4,25 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class EditPage extends StatefulWidget {
-  const EditPage(String id, {super.key});
+  final String id; // Add id parameter to the constructor
+
+  const EditPage(this.id, {Key? key}) : super(key: key);
 
   @override
   State<EditPage> createState() => _EditPageState();
 }
 
-// class _EditPageState extends State<EditPage> {
-//   final db = FirebaseFirestore.instance;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Edit Page'),
-//       ),
-//       body: Center(
-//         child: Text('Edit Page'),
-
-//       ),
 class _EditPageState extends State<EditPage> {
   final db = FirebaseFirestore.instance;
   String noteValue = '';
@@ -35,11 +24,13 @@ class _EditPageState extends State<EditPage> {
 
   Future<void> fetchNoteValue() async {
     try {
-      DocumentSnapshot snapshot =
-          await db.collection('noteapp').doc('your_document_id').get();
+      DocumentSnapshot snapshot = await db
+          .collection('noteapp')
+          .doc(widget.id)
+          .get(); // Use widget.id to access the id parameter
       if (snapshot.exists) {
         setState(() {
-          noteValue = snapshot.data()?['note'] ?? [];
+          noteValue = (snapshot.data() as Map<String, dynamic>)['note'];
         });
       }
     } catch (e) {
@@ -49,10 +40,7 @@ class _EditPageState extends State<EditPage> {
 
   Future<void> updateNoteValue(String newValue) async {
     try {
-      await db
-          .collection('noteapp')
-          .doc('your_document_id')
-          .update({'note': newValue});
+      await db.collection('noteapp').doc(widget.id).update({'note': newValue});
       setState(() {
         noteValue = newValue;
       });
@@ -85,6 +73,3 @@ class _EditPageState extends State<EditPage> {
     );
   }
 }
-//     );
-//   }
-// }
